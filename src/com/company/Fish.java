c4f7fcb14745434fda067ca6a88fcf5821da433f
soraia.carmo <coding_sue@icloud.com>
2018-02-07 10:19:31 +0000

    new Fish class added

--- src/com/company/Fish.java
+++ src/com/company/Fish.java
@@ -7,7 +7,7 @@ public class Fish extends Animal {
     private int fins;
 
     public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
-        super(name, 1, 1 size, weight);
+        super(name, 1, 1, size, weight);
         this.gills = gills;
         this.eyes = eyes;
         this.fins = fins;
